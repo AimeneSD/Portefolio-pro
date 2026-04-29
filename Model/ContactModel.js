7cd15127f26971ae1c5297e56d5af81e26d1e4c0
aimeneSaoud <aimenesaoud@gmail.com>
2026-04-29 11:04:07 +0200

    Passage au port 465 pour SMTP

--- Model/ContactModel.js
+++ Model/ContactModel.js
@@ -15,8 +15,8 @@ class ContactModel {
 
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: userEmail,
         pass: userPass,
